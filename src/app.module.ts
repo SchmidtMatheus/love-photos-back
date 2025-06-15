902652e8f2e76a1349db7d710e9e84926b272f2a
MatheusSchmidt <matheus.schmidt@outlook.com>
2025-06-15 20:20:47 -0300

    refactor: update package dependencies and improve module imports for better organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,15 @@ import { ImportantDatesModule } from './important-dates/important-dates.module';
 import { ShoppingListsModule } from './shopping-lists/shopping-lists.module';
 import { CronModule } from './cron/cron.module';
 import { EmailModule } from './email/email.module';
+import { ImageUploaderModule } from './image-uploader/image-uploader.module';
+import { CollectionsModule } from './collections/collections.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/love-photos'),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/love-photos',
+    ),
     AuthModule,
     UsersModule,
     CouplesModule,
@@ -22,6 +26,8 @@ import { EmailModule } from './email/email.module';
     ShoppingListsModule,
     EmailModule,
     CronModule,
+    ImageUploaderModule,
+    CollectionsModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const dotenv = require('dotenv');
+  dotenv.config();
   app.enableCors();
   await app.listen(3000);
 }
